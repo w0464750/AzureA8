94c152372acc534782962f8b507974d0e4fc5c73
w0464750 <66577598+lilacvenus@users.noreply.github.com>
2022-12-11 23:41:54 -0400

    Init

---
+++ Attempt6/Attempt6.cs
@@ -0,0 +1,62 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Host;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Attempt6
+{
+    public static class Attempt6
+    {
+        public class EventSample
+        {
+            public string EventName { get; set; }
+            public string EventDesc { get; set; }
+            public string EventURL { get; set; }
+        }
+
+        [FunctionName("Attempt6")]
+        public static async Task<HttpResponseMessage> MyFunction( [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req,TraceWriter log)
+        {
+            var event1 = new EventSample
+            {
+                EventName = "Lorem1",
+                EventDesc = "Ipsum",
+                EventURL = "Image.png"
+            };
+
+            var event2 = new EventSample
+            {
+                EventName = "Lorem",
+                EventDesc = "Ipsum",
+                EventURL = "Image.png"
+            };
+
+            var event3 = new EventSample
+            {
+                EventName = "Lorem",
+                EventDesc = "Ipsum",
+                EventURL = "Image.png"
+            };
+
+            EventSample[] eventsArr = { event1, event2, event3 };
+
+            // Serialize the object to a JSON string
+            var json = JsonConvert.SerializeObject(eventsArr);
+
+            // Return the JSON string in the response body
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+        }
+    }
+}
